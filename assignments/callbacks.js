892a1e157f55dee0681bd09b943c232ed823eab7
Maaruf Dauda <mkdauda.work@gmail.com>
2019-08-20 14:56:00 +0100

    Complete stretch challenge for callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -89,4 +89,11 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let unique = [...new Set(array)];
+  // console.log(array)
+  // console.log(unique)
+  return cb(unique)
 }
+
+console.log()
+console.log(removeDuplicates([1,2,3,4,1,3,2,6], noDupli => noDupli))
